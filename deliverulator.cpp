1cba487821eb8a0d8c1db067d8bdd59677738bf7
Mya Chappell <mkchappe@andrew.cmu.edu>
2023-11-15 23:07:55 -0500

    Robot Class
    First draft of robot class and example implementation in deliverulator.cpp

--- deliverulator.cpp
+++ deliverulator.cpp
@@ -10,12 +10,94 @@
 // 	- Rubin Chen
 // ============================================================================================== //
 
+#include <iostream>
 #include "map.h"
 #include "robot.h"
 #include "order.h"
 #include "path-planner.h"
 #include "ui.h"
+#include "fssimplewindow.h"
+#include <random>
 
-int main() {
+
+
+void printVector(std::vector<float> Vec)
+{
+    for (int element : Vec)
+    {
+        std::cout << element << " ";
+    }
+    std::cout << std::endl;
+}
+
+void randomPathVec(std::vector<float>& path)
+{
+    // Seed the random number generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    // Define the range of random numbers
+    std::uniform_int_distribution<int> distribution800(0, 800);
+    std::uniform_int_distribution<int> distribution600(0, 600);
+
+    for (int i = 0; i < 4; ++i)
+    {
+        // Generate and print the first random number
+        int x = distribution800(gen);
+        // Generate and print the second random number
+        int y = distribution600(gen);
+
+        path.push_back(x);
+        path.push_back(y);
+    }
+}
+
+int main()
+{
+    Robot robot[2]; //for now just doing 2 robots
+
+    //create robot ID
+    robot[0].ID = 1;
+    robot[1].ID = 2;
+
+
+    //this is placeholder for path planner
+    randomPathVec(robot[0].path);
+    //printVector(robot[0].path);
+    randomPathVec(robot[1].path);
+
+    FsOpenWindow(0, 0, 800, 600, 1);
+
+    while (FSKEY_NULL == FsInkey())
+    {
+        FsPollDevice();
+
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        //draw a robot
+        robot[0].drawRobot();
+        robot[1].drawRobot();
+
+        //robot tick
+        robot[0].updatePos();
+        robot[1].updatePos();
+
+        //this is placeholder for path planner
+        if (robot[0].path.empty())
+        {
+            randomPathVec(robot[0].path);
+            //printVector(robot[0].path);
+        }
+        if (robot[1].path.empty())
+        {
+            randomPathVec(robot[1].path);
+        }
+
+        FsSwapBuffers();
+
+        FsSleep(10);
+    }
+    
+    
     return 0;
 }
--- include/robot.h
+++ include/robot.h
@@ -10,7 +10,45 @@
 #define ROBOT_H
 
 #include "order.h"
+#include <iostream>
+#include <vector>
 
-class Robot;
+//not sending anything anywhere
+//receiving from Path Planner and UI
+
+/*
+Class Robot
+	x,y coordinates
+	ID
+	path (list of nodes to follow)
+	order ID
+	draw robot
+
+	movement speed
+		function tick (update)
+*/
+
+/* Questions:
+	What data type for ID?
+
+*/
+
+class Robot
+{
+protected:
+	float x = 400, y = 300;
+	float speed = 1;
+
+public:
+	int ID;
+
+	std::vector<float> path; //this will come from path planner
+
+	int order_ID; //this will come from UI
+
+	void drawRobot(void);
+	int updatePos(void);
+
+};
 
 #endif // !defined(ROBOT_H)
