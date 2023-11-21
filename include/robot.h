a428afa131b525f3cf7658100c8458a9130a0bba
Casey Walker <kcmw3e@gmail.com>
2023-11-21 02:23:09 -0500

    updated skeleton for robot

--- include/robot.h
+++ include/robot.h
@@ -9,8 +9,22 @@
 #if !defined(ROBOT_H)
 #define ROBOT_H
 
+#include "path-planner.h"
 #include "order.h"
+#include "map.h"
 
-class Robot;
+class Robot {
+    static constexpr double speed = 0.0;
+
+    double x;
+    double y;
+    Order* order;
+    Path path;
+    Map::Node* last_dest;
+public:
+    void draw();
+    void tick();
+    void assign_order(Order* order);
+};
 
 #endif // !defined(ROBOT_H)
